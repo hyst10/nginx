a3a83f3a0a281307f4705da3a5233ab9bff5c21d
hyst10 <81282704+hyst10@users.noreply.github.com>
2024-07-24 17:50:25 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -19,9 +19,7 @@ MAKE_OPT="./configure --prefix=${APPDIR} \
 --with-pcre \
 --with-stream \
 --with-stream_ssl_module \
---with-stream_realip_module\
---with-cc-opt="-DNGX_SECURE_MEM -I$OPENSSL_LIB/include \
-    -Wno-error=deprecated-declarations""
+--with-stream_realip_module"
 
 _nginx_make_install() {
     wget -c http://nginx.org/download/${PKGNAME}.tar.gz
